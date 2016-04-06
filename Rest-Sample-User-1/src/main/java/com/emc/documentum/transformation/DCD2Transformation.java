220b9f3d963547bdf3b221ca19e289cd02befe89
Mohamed Shaaban <mohamed.shaaban@emc.com>
2016-04-06 12:04:35 +0200

    changed D2 transformation to Documentum Property

--- Rest-Sample-User-1/src/main/java/com/emc/documentum/transformation/DCD2Transformation.java
+++ Rest-Sample-User-1/src/main/java/com/emc/documentum/transformation/DCD2Transformation.java
@@ -7,9 +7,11 @@ import java.util.List;
 import com.emc.d2fs.models.attribute.Attribute;
 import com.emc.d2fs.models.item.Item;
 import com.emc.d2fs.models.node.Node;
+import com.emc.documentum.constants.Cardinality;
 import com.emc.documentum.dtos.DocumentumCabinet;
 import com.emc.documentum.dtos.DocumentumFolder;
 import com.emc.documentum.dtos.DocumentumObject;
+import com.emc.documentum.dtos.DocumentumProperty;
 
 public class DCD2Transformation {
 	private DCD2Transformation(){
@@ -24,7 +26,7 @@ public class DCD2Transformation {
 			object.setType(d2Object.getType());
 			if(d2Object.getAttributes()!=null)
 			{
-				object.setProperties(convertD2Properties(d2Object.getAttributes()));
+				object.setDocProperties(convertD2PropertiesList(d2Object.getAttributes()));
 			}
 		return object;
 	}
@@ -36,9 +38,16 @@ public class DCD2Transformation {
 			object.setType(d2Object.getType());
 			if(d2Object.getAttributes()!=null)
 			{
-				object.setProperties(convertD2Properties(d2Object.getAttributes()));
-				object.setName((String) object.getProperties().get("object_name"));
-				object.getProperties().remove("object_name");
+				object.setDocProperties(convertD2PropertiesList(d2Object.getAttributes()));
+				for(DocumentumProperty property:object.getDocProperties())
+				{
+					if(property.getLocalName().equals("object_name"))
+					{
+						object.setName(property.getValue()+"");
+						object.getDocProperties().remove(property);
+						break;
+					}
+				}
 			}
 		return object;
 	}
@@ -83,6 +92,16 @@ public class DCD2Transformation {
 		for(Attribute attribute:attributes)
 		{
 			properties.put(attribute.getName(), attribute.getValue());
+		}
+		return properties;
+	}
+	public static ArrayList<DocumentumProperty> convertD2PropertiesList(List<Attribute> attributes)
+	{
+		ArrayList<DocumentumProperty>properties = new ArrayList<DocumentumProperty>(); 
+		for(Attribute attribute:attributes)
+		{
+			DocumentumProperty temp = new DocumentumProperty(attribute.getName(), attribute.getName(), attribute.getValue(), Cardinality.Single);
+			properties.add(temp);
 		}
 		return properties;
 	}
