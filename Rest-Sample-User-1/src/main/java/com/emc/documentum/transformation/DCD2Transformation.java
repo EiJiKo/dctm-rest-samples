78875b2d6e148a7c1440bdfa3c095a62d94ed1c2
Ahmed Abdelsamad <ahmed.abdelsamad@emc.com>
2016-04-07 12:31:28 +0200

    Fix: D2 type for Documentum Objects
    Signed-off-by: Ahmed Abdelsamad <ahmed.abdelsamad@emc.com>

--- Rest-Sample-User-1/src/main/java/com/emc/documentum/transformation/DCD2Transformation.java
+++ Rest-Sample-User-1/src/main/java/com/emc/documentum/transformation/DCD2Transformation.java
@@ -7,48 +7,59 @@ import java.util.Map;
 
 import com.emc.d2fs.models.attribute.Attribute;
 import com.emc.d2fs.models.node.Node;
+import com.emc.documentum.dtos.DocumentumCabinet;
+import com.emc.documentum.dtos.DocumentumDocument;
+import com.emc.documentum.dtos.DocumentumFolder;
 import com.emc.documentum.dtos.DocumentumObject;
 
 public class DCD2Transformation {
-	private DCD2Transformation(){
-		
+	private DCD2Transformation() {
+
 	}
-	public static <T extends DocumentumObject> T convertD2Object(Node d2Object, Class<T> type)
-	{
-		T object = null ;
-		try {
-			object = type.newInstance();
-			object.setId(d2Object.getId());
-			object.setName(d2Object.getLabel());
-			object.setType(d2Object.getType());
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
+
+	public static <T extends DocumentumObject> T convertD2Object(Node d2Object, Class<T> type) {
+		T object = null;
+		object = (T) createDocumentumObject(d2Object.getType());
+		object.setId(d2Object.getId());
+		object.setName(d2Object.getLabel());
+		object.setType(d2Object.getType());
+
 		return object;
 	}
-	
-	
-	
 
-	@SuppressWarnings("unchecked")
-	public static <T extends DocumentumObject> ArrayList<T> convertD2ObjectList(List<Node> list,
-			Class<T> class1) {
-		ArrayList<T> documentumObject = new ArrayList<T>();
-		for (Node d2object : list) {
-			documentumObject.add((T) convertD2Object(d2object , class1));
+	private static DocumentumObject createDocumentumObject(String baseTypeId) {
+		DocumentumObject documentumObject;
+		switch (baseTypeId) {
+		case "dm_folder":
+			documentumObject = new DocumentumFolder();
+			break;
+		case "dm_cabinet":
+			documentumObject = new DocumentumCabinet();
+			break;
+		case "dm_document":
+			documentumObject = new DocumentumDocument();
+			break;
+		default:
+			documentumObject = new DocumentumObject();
 		}
 		return documentumObject;
 	}
-	public static Map<String,Object> convertD2Properties(List<Attribute> attributes)
-	{
+
+	@SuppressWarnings("unchecked")
+	public static <T extends DocumentumObject> ArrayList<T> convertD2ObjectList(List<Node> list, Class<T> class1) {
+		ArrayList<T> documentumObject = new ArrayList<T>();
+		for (Node d2object : list) {
+			documentumObject.add((T) convertD2Object(d2object, class1));
+		}
+		return documentumObject;
+	}
+
+	public static Map<String, Object> convertD2Properties(List<Attribute> attributes) {
 		Map<String, Object> properties = new HashMap<String, Object>();
-		for(Attribute attribute:attributes)
-		{
+		for (Attribute attribute : attributes) {
 			properties.put(attribute.getName(), attribute.getValue());
 		}
 		return properties;
 	}
-	
+
 }
