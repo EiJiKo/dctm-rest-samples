8391159334f9eff5123c279d32893718815c126b
Ahmed Abdelsamad <ahmed.abdelsamad@emc.com>
2016-04-07 12:13:08 +0200

    Updated: Swagger config to include only service controllers
    Signed-off-by: Ahmed Abdelsamad <ahmed.abdelsamad@emc.com>

--- Rest-Sample-User-1/src/main/java/com/emc/documentum/configuration/SwaggerConfig.java
+++ Rest-Sample-User-1/src/main/java/com/emc/documentum/configuration/SwaggerConfig.java
@@ -15,7 +15,9 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.emc.documentum.services.rest"))
 				.paths(PathSelectors.any()).build();
+
 	}
 }
